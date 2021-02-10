704a3cf143ce70ef659c8edfe567eccb47caf622
CapeSathish <77102126+CapeSathish@users.noreply.github.com>
2021-02-10 19:19:50 +0530

    Integration with spring boot

--- src/app/services/loginservice.service.ts
+++ src/app/services/loginservice.service.ts
@@ -1,25 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
-
+import {environment} from '../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class LoginserviceService {
+  apiUrl = environment.apiUrl
+  constructor(private http: HttpClient) { }
 
-  constructor ( private http: HttpClient) { }
-  
-  login (username: String ,password: String) {
-    return this.http.post<any>(`/loginsuccess`, { username: username, password: password })
-    .pipe(map(user => {
+  login(email: String, password: String) {
+    return this.http.post<any>(`$apiUrl/authenticate`, { email: email, password: password })
+      .pipe(map(user => {
         if (user && user.token) {
-            localStorage.setItem('currentUser', JSON.stringify(user));
+          localStorage.setItem('currentUser', JSON.stringify(user));
         }
 
         return user;
-    }));
-}
+      }));
   }
+}
 
 
 
