b13728c2cc71e26f01c8672dda10527060361a30
CapeSathish <77102126+CapeSathish@users.noreply.github.com>
2021-02-10 19:25:07 +0530

    Code clean up

--- src/app/services/loginservice.service.ts
+++ src/app/services/loginservice.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
-import {environment} from '../../environments/environment';
+import { environment } from '../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
@@ -9,19 +9,15 @@ export class LoginserviceService {
   apiUrl = environment.apiUrl
   constructor(private http: HttpClient) { }
 
-  
-  login (email: String ,password: String) {
-    return this.http.post<any>(`$apiUrl/authenticate`, { email: email, password: password })
-    .pipe(map(email => {
-        if (email && email.token) {
-            localStorage.setItem('currentUser', JSON.stringify(email));
-        }
 
+  login(email: String, password: String) {
+    return this.http.post<any>(`$apiUrl/authenticate`, { email: email, password: password })
+      .pipe(map(email => {
+        if (email && email.token) {
+          localStorage.setItem('currentUser', JSON.stringify(email));
+        }
         return email;
-    }));
+      }));
 
   }
 }
-
-
-
