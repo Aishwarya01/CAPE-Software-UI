3453e28c17f7ef92c296a8903d17a9679dac4e49
CapeSathish <77102126+CapeSathish@users.noreply.github.com>
2021-02-11 19:38:38 +0530

    Added for calling the service backend

--- src/app/services/loginservice.service.ts
+++ src/app/services/loginservice.service.ts
@@ -12,12 +12,7 @@ export class LoginserviceService {
   apiUrl = environment.apiUrl
   constructor ( private http: HttpClient) { }
   
-  public login (user :User): Observable<any> {
-    return this.http.post<any>("http://localhost:8080/login", user)
-  }
-}
-
-
-
+  public login(user :User): Observable<any> {
+    return this.http.post<any>('apiUrl/autheticate', user)
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   environmentName: 'development',
-  apiUrl: 'http://localhost:8080'
+  apiUrl: 'http://localhost:8080/api/v1'
 };
 
 /*
