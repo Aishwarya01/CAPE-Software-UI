a8834d735ba5061c58cb7a8f7d5310904bf63b30
CapeSathish <77102126+CapeSathish@users.noreply.github.com>
2021-02-24 19:24:30 +0530

    Modified the index.html path in server.js

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-app.use(express.static(__dirname + '/lv-inspection-safety-ui'));
-app.get('/*', function(req,res){
-res.sendFile(path.join(__dirname, 'lv-inspection-safety-ui', 'index.html'))
-});
+// app.use(express.static(__dirname + '/lv-inspection-safety-ui'));
+// app.get('/*', function(req,res){
+// res.sendFile(path.join(__dirname, 'lv-inspection-safety-ui', 'index.html'))
+// });
+app.use(express.static('./dist/lv-inspection-safety-ui'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/lv-inspection-safety-ui/'}),
+);
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 4200);
