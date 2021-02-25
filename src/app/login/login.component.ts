cac704096ec9125bc7fa1ae05bfb0748393c71f6
CapeSathish <77102126+CapeSathish@users.noreply.github.com>
2021-02-25 17:46:33 +0530

    Fixed for login issues

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl } from  '@angular/forms';
-import { Router,ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
 import { User } from '../model/user';
 import { LoginserviceService } from '../services/loginservice.service';
 
@@ -14,53 +14,52 @@ export class LoginComponent implements OnInit {
     email: new FormControl(''),
     password: new FormControl('')
   });
-  
+
   loading = false;
   submitted = false;
   user = new User();
-  msg="";
+  msg = "";
 
   constructor(
     private formBuilder: FormBuilder,
     private router: Router,
     private loginservice: LoginserviceService
-    ) {}
-  
+  ) { }
+
 
   ngOnInit() {
-    this.loginForm  =  this.formBuilder.group({
+    this.loginForm = this.formBuilder.group({
       email: ['', [
         Validators.required,
         Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
       password: ['', Validators.required]
-  });
+    });
   }
 
-  get f(){
+  get f() {
     return this.loginForm.controls;
   }
 
   onSubmit() {
-    this.submitted=true;
-    
+    this.submitted = true;
+
     //breaks here if form is invalid
-    if(this.loginForm.invalid) {
+    if (this.loginForm.invalid) {
       return;
     }
-
-    this.loading=true;
-    this.loginservice.login(this.user.email,this.user.password).subscribe(
-      data=> { 
+    this.user.userName = this.user.email;
+    console.log(this.user.userName)
+    this.loading = true;
+    this.loginservice.login(this.user.userName, this.user.password).subscribe(
+      data => {
         console.log("Response Success");
         this.router.navigate(['/home'])
       },
       error => {
         console.log("Exception occured");
-       this.msg="Please enter a valid Email and Password";
+        this.msg = "Please enter a valid Email and Password";
       }
     )
-    
-    }
+
   }
-
-
+}
--- src/app/services/loginservice.service.ts
+++ src/app/services/loginservice.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
-import { HttpClient , HttpHeaders} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { User } from '../model/user';
 import { environment } from 'src/environments/environment';
 
 
-const httpoption ={
+const httpoption = {
   headers: new HttpHeaders({
-    'Content-Type' : 'application/json',
+    'Content-Type': 'application/json',
   })
 };
 
@@ -17,22 +17,17 @@ const httpoption ={
 })
 export class LoginserviceService {
   apiUrl = environment.apiUrl;
- 
-
-
-  constructor ( private http: HttpClient) { }
-
-  public login(email: String,password: String): Observable<any> {
-    return this.http.post<any>(this.apiUrl+'/autheticate', {email,password}, httpoption)
-   .pipe(map(user => {
-      localStorage.setItem('currentUser', JSON.stringify(user));
-      console.log(localStorage.getItem('currentUser'));
-      return user;
-    }));
+  constructor(private http: HttpClient) { }
+  public login(userName: String, password: String): Observable<any> {
+    return this.http.post<any>(this.apiUrl + '/authenticate', { userName, password }, httpoption)
+      .pipe(map(user => {
+        localStorage.setItem('currentUser', JSON.stringify(user));
+        return user;
+      }));
   }
 
- public logout() {
+  public logout() {
     localStorage.removeItem('currentUser');
-   console.log(localStorage.getItem('currentUser'));
-}
+    console.log(localStorage.getItem('currentUser'));
+  }
 }
