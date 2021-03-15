e96ad8ba6c4e891d3dc4c37725c9b5b8d429b457
Arunkumar-CAPE <arunkumar.k@capeindia.net>
2021-03-15 19:43:34 +0530

    header added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,7 @@
+import { BreakpointObserver } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LoginserviceService } from '../services/loginservice.service';
 
 @Component({
   selector: 'app-header',
@@ -6,8 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  
+  email: String = '';
 
-  constructor() { }
+  constructor(private loginservice: LoginserviceService,
+              private router: ActivatedRoute,
+              private route: Router) {
+              this.email=this.router.snapshot.paramMap.get('email') || '{}'
+            }
+
+    logout(){
+      this.loginservice.logout();
+      this.route.navigate(['login']);
+    }
+  
+    changePassword(email: String){
+      this.route.navigate(['changePassword', {email: email}])
+    }
+  
+    profileUpdate(email: String) {
+      this.route.navigate(['profile', {email: email}])
+    }
 
   ngOnInit(): void {
   }
