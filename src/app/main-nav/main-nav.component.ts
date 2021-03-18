e7a3aeb4e0df576be4ab453ea7cc60ca373284e7
CapeSathish <77102126+CapeSathish@users.noreply.github.com>
2021-03-18 11:30:38 +0530

    Dynamic Loading of Application Types from Service

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -8,6 +8,8 @@ import { LvInspectionDetailsComponent } from '../lv-inspection-details/lv-inspec
 import { ComponentFactoryResolver } from '@angular/core';
 import { RiskAssessmentInspectionMaintenanceComponent } from '../risk-assessment-inspection-maintenance/risk-assessment-inspection-maintenance.component';
 import { EmcAssessmentInstallationComponent } from '../emc-assessment-installation/emc-assessment-installation.component';
+import { MainNavService } from '../services/main-nav.service';
+import { ApplicationType } from '../model/applicationtype';
 
 @Component({
   selector: 'app-main-nav',
@@ -22,22 +24,22 @@ export class MainNavComponent {
       map(result => result.matches),
       shareReplay()
     );
-    selectedOptions = [{name: 'Verification Of LV Systems (IEC 60364-6)', id:1}];
-    applicationTypes: {name: string, id: number }[] = [
-      {name: 'Verification Of LV Systems (IEC 60364-6)', id: 1}, 
-      {name: 'Verification of HV system (up to 33 kV) (IEC 61936-1)', id: 2},
-      {name: 'Lightning protection conformity assessment, risk assessment, inspection and maintenance (IEC 62305-3 & 4)', id: 3 },
-      {name: 'EMC assessment of an installation (IEC 61000-5-1)', id: 4},
-      {name: 'Failure analysis of electronic systems', id:5},
-      {name: 'Conformity and project analysis', id:6}
-    ];
+    
+  applicationTypes: ApplicationType[] = [];
   email: String = '';
   constructor(private breakpointObserver: BreakpointObserver,
     private loginservice: LoginserviceService,
     private router: ActivatedRoute,
     private route: Router,
-    private componentFactoryResolver: ComponentFactoryResolver) {
-    this.email = this.router.snapshot.paramMap.get('email') || '{}'
+    private componentFactoryResolver: ComponentFactoryResolver,
+    private mainNavService: MainNavService) {
+    this.email = this.router.snapshot.paramMap.get('email') || '{}';
+
+    this.mainNavService.retrieveApplicationTypes().subscribe(
+      data=> {
+        this.applicationTypes = data;
+      }
+    )
   }
   
   logout() {
---
+++ src/app/services/main-nav.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { ApplicationType } from '../model/applicationtype';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MainNavService {
+
+  apiUrl = environment.apiUrl;
+  constructor(private http: HttpClient) { }
+
+  public retrieveApplicationTypes(): Observable<any>{
+    return this.http.get<ApplicationType>(this.apiUrl+'/retrieveApplicationTypes')
+  }
+  
+}
