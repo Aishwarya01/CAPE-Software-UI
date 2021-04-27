453f9801093184d2be22cc731015110ef1b3e2ce
CapeSathish <77102126+CapeSathish@users.noreply.github.com>
2021-04-27 18:52:09 +0530

    Fix for deleting the department

--- src/app/services/department.service.ts
+++ src/app/services/department.service.ts
@@ -22,7 +22,7 @@ export class DepartmentService {
   }
 
   public deleteDepartment(email: String, departmentId: number, ): Observable<any> {
-    return this.http.delete<any>(this.apiUrl + '/deleteCompany' + '/' + email + '/' + departmentId)
+    return this.http.delete<any>(this.apiUrl + '/deleteDepartment' + '/' + email + '/' + departmentId)
   }
 
   public retrieveDepartment(email: String, company: Company): Observable<any> {
