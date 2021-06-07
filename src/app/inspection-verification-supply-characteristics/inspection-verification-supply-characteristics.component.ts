ea26d2f0c5acf3a30321fe722654c6bd75a30061
Arunkumar-CAPE <78420626+Arunkumar-CAPE@users.noreply.github.com>
2021-06-07 19:50:06 +0530

    Revert "some changes on html"
    This reverts commit 6d4a83f2d29da9b41718be2613e6d453ec673424.

--- src/app/inspection-verification-supply-characteristics/inspection-verification-supply-characteristics.component.ts
+++ src/app/inspection-verification-supply-characteristics/inspection-verification-supply-characteristics.component.ts
@@ -31,17 +31,17 @@ export class InspectionVerificationSupplyCharacteristicsComponent implements OnI
 
 
   panelOpenState = false;
-  systemEarthingList: String[]= ['TN-C','TN-C-S','TN-S','IT','TT','To be verified'];
+  systemEarthingList: String[]= ['TN-C','TN-C-S','TN-S','IT','TT'];
   liveConductorACList:String[]=['1-phase, 2-wire (LN)','1-phase, 3-wire (LLM)','2-phase, 3-wire (LLN)','3-phase, 3-wire (LLL)','3-phase, 4-wire (LLLN)'];
   liveConductorDCList:String[]=['2-pole','3-pole','Others'];
   ProtectiveDevicelist:string[]=['Fuse','MCB','MCCB','ACB'];
-  AlternatesupplyList:string[]=['Yes','No'];
+  AlternatesupplyList:string[]=['yes','No'];
   MeansofEarthingList:string[]=['Suppliers facility',' Installation earth electrode'];
   electrodeTypeList:string[]=['Vertical','Horizontal','Combined vertical + horizontal'];
   electrodeMaterialList:string[]=['Copper','Coppebondedr  steel','Galvanised steel','Combination','Others'];
-  conductorVerifyList:string[]=['Yes','No'];
-  bondingConductorVerifyList:string[]=['Yes','No'];
-  earthingConductorVerifyList:string[]=['Yes','No'];
+  conductorVerifyList:string[]=['yes','No'];
+  bondingConductorVerifyList:string[]=['yes','No'];
+  earthingConductorVerifyList:string[]=['yes','No'];
 
   supplycharesteristicForm = new FormGroup({
     // systemEarthing: new FormControl(''),
@@ -53,7 +53,6 @@ export class InspectionVerificationSupplyCharacteristicsComponent implements OnI
   })
   sources: boolean=false;
   breaker: boolean=false;
-  enableRCBO: boolean;
 
   constructor(private supplyCharacteristicsService: SupplyCharacteristicsService,
               private formBuilder: FormBuilder) { }
@@ -336,16 +335,6 @@ export class InspectionVerificationSupplyCharacteristicsComponent implements OnI
       this.table2AC = false;
 
     }
-  }
-  changeCurrent3(e: any) {
-    let changedValue = e.target.value;
-   if(changedValue == "RCBO") {
-      this.enableRCBO = true;
-    }
-    else {
-    
-      this.enableRCBO = false;
-  }
   }
 
   onKeyAlernate(event: KeyboardEvent)    {
