4857763f2dcf99cf3b1b1cf312847d27a6b76fdb
CAPE-aishwarya <aishwarya@capeindia.net>
2021-06-25 18:11:06 +0530

    main-nav issue fixed

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -47,13 +47,13 @@ export class MainNavComponent implements OnInit, OnDestroy {
       map(result => result.matches),
       shareReplay()
     );
-    @ViewChild('ref1', { read: ViewContainerRef })
-    viewContainerRef1!: ViewContainerRef;
-    isHandset1$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-      .pipe(
-        map(result => result.matches),
-        shareReplay()
-      );
+    // @ViewChild('ref1', { read: ViewContainerRef })
+    // viewContainerRef1!: ViewContainerRef;
+    // isHandset1$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    //   .pipe(
+    //     map(result => result.matches),
+    //     shareReplay()
+    //   );
   
   applicationTypes: ApplicationType[] = [];
   @Output() passEntry: EventEmitter<any> = new EventEmitter();
@@ -76,7 +76,7 @@ export class MainNavComponent implements OnInit, OnDestroy {
   welcome: boolean = true;
   //isExpanded: any;
   //isExpanded: any;
-  selectedRowIndex = -1;
+  selectedRowIndex = 0;
 
 
 
@@ -175,34 +175,38 @@ export class MainNavComponent implements OnInit, OnDestroy {
     switch (id) {
       case 1:
         this.viewContainerRef.clear();
-        this.viewContainerRef1.clear();
+        //this.viewContainerRef1.clear();
         const lvInspectionFactory = this.componentFactoryResolver.resolveComponentFactory(LvInspectionDetailsComponent);
         const lvInspectionRef = this.viewContainerRef.createComponent(lvInspectionFactory);
-        const lvInspectionRef1 = this.viewContainerRef1.createComponent(lvInspectionFactory);
+        //const lvInspectionRef1 = this.viewContainerRef1.createComponent(lvInspectionFactory);
         lvInspectionRef.changeDetectorRef.detectChanges();
-        lvInspectionRef1.changeDetectorRef.detectChanges();
+        //lvInspectionRef1.changeDetectorRef.detectChanges();
         break;
       case 2:
         this.viewContainerRef.clear();
-        this.viewContainerRef1.clear();
+        //this.viewContainerRef1.clear();
         break;
       case 3:
         this.viewContainerRef.clear();
+        //this.viewContainerRef1.clear();
         const riskAssessmentInspectionFactory = this.componentFactoryResolver.resolveComponentFactory(RiskAssessmentInspectionMaintenanceComponent);
         const riskAssessmentInspectionRef = this.viewContainerRef.createComponent(riskAssessmentInspectionFactory);
         riskAssessmentInspectionRef.changeDetectorRef.detectChanges();
         break;
       case 4:
         this.viewContainerRef.clear();
+        //this.viewContainerRef1.clear();
         const emcAssessmentInspectionFactory = this.componentFactoryResolver.resolveComponentFactory(EmcAssessmentInstallationComponent);
         const emcAssessmentInspectionRef = this.viewContainerRef.createComponent(emcAssessmentInspectionFactory);
         emcAssessmentInspectionRef.changeDetectorRef.detectChanges();
         break;
       case 5:
         this.viewContainerRef.clear();
+        //this.viewContainerRef1.clear();
         break;
       case 6:
         this.viewContainerRef.clear();
+        //this.viewContainerRef1.clear();
         break;
     }
   }
