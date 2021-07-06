4598b9e34628c8bee17b8081dba0508a90ac42e9
Arunkumar-CAPE <78420626+Arunkumar-CAPE@users.noreply.github.com>
2021-07-06 19:35:12 +0530

    Update inspection-verification-supply-characteristics.component.ts

--- src/app/inspection-verification-supply-characteristics/inspection-verification-supply-characteristics.component.ts
+++ src/app/inspection-verification-supply-characteristics/inspection-verification-supply-characteristics.component.ts
@@ -778,9 +778,7 @@ export class InspectionVerificationSupplyCharacteristicsComponent implements OnI
   }
   
 nextTab2() {
-    // this.supplycharesteristic.siteId = this.service.siteCount;
-    this.supplycharesteristic.siteId = 17;
-
+    this.supplycharesteristic.siteId = this.service.siteCount;
     this.supplycharesteristic.userName = this.email;
     this.submitted = true;
     if(this.supplycharesteristicForm.invalid) {
