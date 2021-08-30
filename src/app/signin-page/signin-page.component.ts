1f3ee348a9c8ea13cd263f1e968207d9f9a424ff
CAPE-aishwarya <84174657+CAPE-aishwarya@users.noreply.github.com>
2021-08-30 13:13:12 +0530

    dynamic content from env.ts file

--- src/app/signin-page/signin-page.component.ts
+++ src/app/signin-page/signin-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-signin-page',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SingInPageComponent implements OnInit {
 
+ signInContent :string = environment.signInContent;
+ 
   constructor() { }
 
   ngOnInit(): void {
-  }
+  
+   }
 
 }
