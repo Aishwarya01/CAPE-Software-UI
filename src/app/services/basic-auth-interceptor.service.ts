1901aafc3156363e07a6309a4d4fe084331e6ca9
CapeSathish <77102126+CapeSathish@users.noreply.github.com>
2021-09-30 08:12:38 +0530

    Revert "Modified the basic-auth header changes"
    This reverts commit d9464bc3d753b2bc06ee3231e0b31a8a3b746f33.

--- src/app/services/basic-auth-interceptor.service.ts
+++ src/app/services/basic-auth-interceptor.service.ts
@@ -16,10 +16,6 @@ export class BasicAuthHtppInterceptorService implements HttpInterceptor {
         const authReq = req.clone({
             headers: new HttpHeaders({
                 'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Credentials': 'true',
-                'Access-Control-Allow-Headers': 'Content-Type',
-                'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
                 'Authorization': `Bearer ${this.loginService.token}`
             })
         });
