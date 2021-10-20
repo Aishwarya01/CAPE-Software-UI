cdb4139b8b05e81d24c5b43ad89776dc80f88711
capeelangovan <86285794+capeelangovan@users.noreply.github.com>
2021-10-20 12:38:24 +0530

    lps_basic_page

---
+++ src/app/lps-basic-page/lps-basic-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lps-basic-page',
+  templateUrl: './lps-basic-page.component.html',
+  styleUrls: ['./lps-basic-page.component.css']
+})
+export class LpsBasicPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
