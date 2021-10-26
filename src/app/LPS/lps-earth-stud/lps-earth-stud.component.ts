978325a4d8d747ea8f47439566373af9e1d6bea3
capeelangovan <86285794+capeelangovan@users.noreply.github.com>
2021-10-26 14:16:10 +0530

    new_comp_created

---
+++ src/app/LPS/lps-earth-stud/lps-earth-stud.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lps-earth-stud',
+  templateUrl: './lps-earth-stud.component.html',
+  styleUrls: ['./lps-earth-stud.component.css']
+})
+export class LpsEarthStudComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/LPS/lps-seperation-distance/lps-seperation-distance.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lps-seperation-distance',
+  templateUrl: './lps-seperation-distance.component.html',
+  styleUrls: ['./lps-seperation-distance.component.css']
+})
+export class LpsSeperationDistanceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
