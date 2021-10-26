e1f11515ca301961fabd78dd72e1d2d833926b19
capeelangovan <86285794+capeelangovan@users.noreply.github.com>
2021-10-26 11:27:21 +0530

    itration_added

--- src/app/LPS/lps-air-termination/lps-air-termination.component.ts
+++ src/app/LPS/lps-air-termination/lps-air-termination.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-lps-air-termination',
@@ -9,21 +9,242 @@ import { AbstractControl, FormArray, FormBuilder, FormGroup } from '@angular/for
 export class LpsAirTerminationComponent implements OnInit {
 
 
-  airTerminationfrom!: FormGroup;
+  airTerminationForm!: FormGroup;
+  vatArr!: FormArray;
+  meshArr!: FormArray;
+  holderArr!: FormArray;
+  clampArr!: FormArray;
+  expArr!: FormArray;
+  conArr!: FormArray;
 
-  testaccordianArr!: FormArray;
-
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
-    this.airTerminationfrom = this.formBuilder.group({
-      testaccordianArr: this.formBuilder.array([])
-      
+    this.airTerminationForm = this.formBuilder.group({
+      vatArr: this.formBuilder.array([this.createVatArrForm()]),
+      meshArr: this.formBuilder.array([this.createMeshArrForm()]),
+      holderArr: this.formBuilder.array([this.createHolderArrForm()]),
+
+      clampArr: this.formBuilder.array([this.createClampArrForm()]),
+      expArr: this.formBuilder.array([this.createExpansioArrForm()]),
+      conArr: this.formBuilder.array([this.createConArrForm()])
     });
   }
 
-  getTestControls(): AbstractControl[] {
-    return (<FormArray>this.airTerminationfrom.get('testaccordianArr')).controls;
+  vatControls(): AbstractControl[] {
+    return (<FormArray>this.airTerminationForm.get('vatArr')).controls;
+  }
+
+  meshControls(): AbstractControl[] {
+    return (<FormArray>this.airTerminationForm.get('meshArr')).controls;
+  }
+
+  holdersContols(): AbstractControl[] {
+    return (<FormArray>this.airTerminationForm.get('holderArr')).controls;
+  }
+
+  clampsControls(): AbstractControl[] {
+    return (<FormArray>this.airTerminationForm.get('clampArr')).controls;
+  }
+
+  expansionControls(): AbstractControl[] {
+    return (<FormArray>this.airTerminationForm.get('expArr')).controls;
+  }
+
+  connectorsControls(): AbstractControl[] {
+    return (<FormArray>this.airTerminationForm.get('conArr')).controls;
+  }
+
+  private createVatArrForm(): FormGroup{
+    return new FormGroup({
+      vatobs1: new FormControl(''),
+      vatrem1: new FormControl(''),
+      vatobs2: new FormControl(''),
+      vatrem2: new FormControl(''),
+      vatobs3: new FormControl(''),
+      vatrem3: new FormControl(''),
+      vatobs4: new FormControl(''),
+      vatrem4: new FormControl(''),
+      vatobs5: new FormControl(''),
+      vatrem5: new FormControl(''),
+      vatobs6: new FormControl(''),
+      vatrem6: new FormControl(''),
+      vatobs7: new FormControl(''),
+      vatrem7: new FormControl(''),
+      vatobs8: new FormControl(''),
+      vatrem8: new FormControl(''),
+      vatobs9: new FormControl(''),
+      vatrem9: new FormControl(''),
+      vatobs10: new FormControl(''),
+      vatrem10: new FormControl(''),
+      vatobs11: new FormControl(''),
+      vatrem11: new FormControl(''),
+      vatobs12: new FormControl(''),
+      vatrem12: new FormControl(''),
+      vatobs13: new FormControl(''),
+      vatrem13: new FormControl(''),
+    })
+  }
+
+  private createMeshArrForm(): FormGroup{
+    return new FormGroup({
+      meshobs1: new FormControl(''),
+      meshrem1: new FormControl(''),
+      meshobs2: new FormControl(''),
+      meshrem2: new FormControl(''),
+      meshobs3: new FormControl(''),
+      meshrem3: new FormControl(''),
+      meshobs4: new FormControl(''),
+      meshrem4: new FormControl(''),
+      meshobs5: new FormControl(''),
+      meshrem5: new FormControl(''),
+      meshobs6: new FormControl(''),
+      meshrem6: new FormControl(''),
+      meshobs7: new FormControl(''),
+      meshrem7: new FormControl(''),
+    })
+  }
+
+  private createHolderArrForm(): FormGroup{
+    return new FormGroup({
+      holdersobs1: new FormControl(''),
+      holdersrem1: new FormControl(''),
+      holdersobs2: new FormControl(''),
+      holdersrem2: new FormControl(''),
+      holdersobs3: new FormControl(''),
+      holdersrem3: new FormControl(''),
+      holdersobs4: new FormControl(''),
+      holdersrem4: new FormControl(''),
+      holdersobs5: new FormControl(''),
+      holdersrem5: new FormControl(''),
+      holdersobs6: new FormControl(''),
+      holdersrem6: new FormControl(''),
+      holdersobs7: new FormControl(''),
+      holdersrem7: new FormControl(''),
+      holdersobs8: new FormControl(''),
+      holdersrem8: new FormControl(''),
+      holdersobs9: new FormControl(''),
+      holdersrem9: new FormControl(''),
+      holdersobs10: new FormControl(''),
+      holdersrem10: new FormControl(''),
+      holdersobs11: new FormControl(''),
+      holdersrem11: new FormControl(''),
+      holdersobs12: new FormControl(''),
+      holdersrem12: new FormControl(''),
+      holdersobs13: new FormControl(''),
+      holdersrem13: new FormControl(''),
+      holdersobs14: new FormControl(''),
+      holdersrem14: new FormControl(''),
+      holdersobs15: new FormControl(''),
+      holdersrem15: new FormControl('')
+    }) 
+  }
+
+  private createClampArrForm(): FormGroup{
+    return new FormGroup({
+      clampobs1: new FormControl(''),
+      clamprem1: new FormControl(''),
+      clampobs2: new FormControl(''),
+      clamprem2: new FormControl(''),
+      clampobs3: new FormControl(''),
+      clamprem3: new FormControl(''),
+      clampobs4: new FormControl(''),
+      clamprem4: new FormControl(''),
+      clampobs5: new FormControl(''),
+      clamprem5: new FormControl(''),
+      clampobs6: new FormControl(''),
+      clamprem6: new FormControl(''),
+      clampobs7: new FormControl(''),
+      clamprem7: new FormControl(''),
+      clampobs8: new FormControl(''),
+      clamprem8: new FormControl(''),
+      clampobs9: new FormControl(''),
+      clamprem9: new FormControl('')
+    })
+  }
+
+  private createExpansioArrForm(): FormGroup{
+    return new FormGroup({
+      expobs1: new FormControl(''),
+      exprem1: new FormControl(''),
+      expobs2: new FormControl(''),
+      exprem2: new FormControl(''),
+      expobs3: new FormControl(''),
+      exprem3: new FormControl(''),
+      expobs4: new FormControl(''),
+      exprem4: new FormControl(''),
+      expobs5: new FormControl(''),
+      exprem5: new FormControl(''),
+      expobs6: new FormControl(''),
+      exprem6: new FormControl(''),
+      expobs7: new FormControl(''),
+      exprem7: new FormControl('')
+    })
+  }
+  
+  private createConArrForm(): FormGroup{
+    return new FormGroup({
+      conobs1: new FormControl(''),
+      conrem1: new FormControl(''),
+      conobs2: new FormControl(''),
+      conrem2: new FormControl(''),
+      conobs3: new FormControl(''),
+      conrem3: new FormControl(''),
+      conobs4: new FormControl(''),
+      conrem4: new FormControl(''),
+      conobs5: new FormControl(''),
+      conrem5: new FormControl(''),
+      conobs6: new FormControl(''),
+      conrem6: new FormControl(''),
+      conobs7: new FormControl(''),
+      conrem7: new FormControl(''),
+      conobs8: new FormControl(''),
+      conrem8: new FormControl(''),
+      conobs9: new FormControl(''),
+      conrem9: new FormControl(''),
+      conobs10: new FormControl(''),
+      conrem10: new FormControl(''),
+      conobs11: new FormControl(''),
+      conrem11: new FormControl('')
+    })
+  }
+
+  submit(){
+    this.vatArr = this.airTerminationForm.get('vatArr') as FormArray;
+    this.vatArr.push(this.createVatArrForm());
+    console.log(this.airTerminationForm)
+  }
+
+  submit1(){
+    this.meshArr = this.airTerminationForm.get('meshArr') as FormArray;
+    this.meshArr.push(this.createMeshArrForm());
+    console.log(this.airTerminationForm)
+  }
+
+  submit2(){
+    this.holderArr = this.airTerminationForm.get('holderArr') as FormArray;
+    this.holderArr.push(this.createHolderArrForm());
+    console.log(this.airTerminationForm)
+  }
+
+  submit3(){
+    this.clampArr = this.airTerminationForm.get('clampArr') as FormArray;
+    this.clampArr.push(this.createClampArrForm());
+    console.log(this.airTerminationForm)
+  }
+
+  submit4(){
+    this.expArr = this.airTerminationForm.get('expArr') as FormArray;
+    this.expArr.push(this.createExpansioArrForm());
+    console.log(this.airTerminationForm)
+  }
+
+  submit5(){
+    this.conArr = this.airTerminationForm.get('conArr') as FormArray;
+    this.conArr.push(this.createConArrForm());
+    console.log(this.airTerminationForm)
   }
 
 }
--- src/app/LPS/lps-down-conductors/lps-down-conductors.component.ts
+++ src/app/LPS/lps-down-conductors/lps-down-conductors.component.ts
@@ -9,7 +9,12 @@ import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup } from 
 export class LpsDownConductorsComponent implements OnInit {
 
   downConductorForm! : FormGroup;
+  
   downArr! : FormArray;
+  bridgingArr!: FormArray;
+  holderArr!: FormArray;
+  connectorArr!: FormArray;
+  lightArr!: FormArray;
 
   constructor(
     private formBuilder : FormBuilder,
@@ -17,7 +22,11 @@ export class LpsDownConductorsComponent implements OnInit {
 
   ngOnInit(): void {
     this.downConductorForm = this.formBuilder.group({
-    downArr: this.formBuilder.array([this.createDownArrForm()])
+    downArr: this.formBuilder.array([this.createDownArrForm()]),
+    bridgingArr: this.formBuilder.array([this.createBridgeArrForm()]),
+    holderArr: this.formBuilder.array([this.createHolderArrForm()]),
+    connectorArr: this.formBuilder.array([this.createConnectorArrForm()]),
+    lightArr: this.formBuilder.array([this.createLightArrForm()])
   });
   }
 
@@ -25,6 +34,21 @@ export class LpsDownConductorsComponent implements OnInit {
     return (<FormArray> this.downConductorForm.get('downArr')).controls
   }
 
+  bridgingCablesControls(): AbstractControl[] {
+    return (<FormArray> this.downConductorForm.get('bridgingArr')).controls
+  }
+
+  holdersControls(): AbstractControl[] {
+    return (<FormArray> this.downConductorForm.get('holderArr')).controls
+  }
+
+  connectorsControls(): AbstractControl[] {
+    return (<FormArray> this.downConductorForm.get('connectorArr')).controls
+  }
+
+  lightControls(): AbstractControl[] {
+    return (<FormArray> this.downConductorForm.get('lightArr')).controls
+  }
   private createDownArrForm(): FormGroup {
 
     return new FormGroup({
@@ -61,10 +85,121 @@ export class LpsDownConductorsComponent implements OnInit {
     })
   }
 
+  private createBridgeArrForm(): FormGroup {
+    
+    return new FormGroup({
+      obs1: new FormControl(''),
+      rem1: new FormControl(''),
+      obs2: new FormControl(''),
+      rem2: new FormControl(''),
+      obs3: new FormControl(''),
+      rem3: new FormControl(''),
+      obs4: new FormControl(''),
+      rem4: new FormControl(''),
+      obs5: new FormControl(''),
+      rem5: new FormControl(''),
+      obs6: new FormControl(''),
+      rem6: new FormControl(''),
+      obs7: new FormControl(''),
+      rem7: new FormControl('')
+    })
+  }
+
+  private createHolderArrForm(): FormGroup {
+    
+    return new FormGroup({
+      holderObs1: new FormControl(''),
+      holderRem1: new FormControl(''),
+      holderObs2: new FormControl(''),
+      holderRem2: new FormControl(''),
+      holderObs3: new FormControl(''),
+      holderRem3: new FormControl(''),
+      holderObs4: new FormControl(''),
+      holderRem4: new FormControl(''),
+      holderObs5: new FormControl(''),
+      holderRem5: new FormControl(''),
+      holderObs6: new FormControl(''),
+      holderRem6: new FormControl(''),
+      holderObs7: new FormControl(''),
+      holderRem7: new FormControl(''),
+      holderObs8: new FormControl(''),
+      holderRem8: new FormControl('')
+    })
+  }
+
+  private createConnectorArrForm(): FormGroup {
+    
+    return new FormGroup({
+      cobs1: new FormControl(''),
+      crem1: new FormControl(''),
+      cobs2: new FormControl(''),
+      crem2: new FormControl(''),
+      cobs3: new FormControl(''),
+      crem3: new FormControl(''),
+      cobs4: new FormControl(''),
+      crem4: new FormControl(''),
+      cobs5: new FormControl(''),
+      crem5: new FormControl(''),
+      cobs6: new FormControl(''),
+      crem6: new FormControl(''),
+      cobs7: new FormControl(''),
+      crem7: new FormControl(''),
+      cobs8: new FormControl(''),
+      crem8: new FormControl('')
+    })
+  }
+
+  private createLightArrForm(): FormGroup {
+    return new FormGroup({
+      lobs1: new FormControl(''),
+      lrem1: new FormControl(''),
+      lobs2: new FormControl(''),
+      lrem2: new FormControl(''),
+      lobs3: new FormControl(''),
+      lrem3: new FormControl(''),
+      lobs4: new FormControl(''),
+      lrem4: new FormControl(''),
+      lobs5: new FormControl(''),
+      lrem5: new FormControl(''),
+      lobs6: new FormControl(''),
+      lrem6: new FormControl(''),
+      lobs7: new FormControl(''),
+      lrem7: new FormControl(''),
+      lobs8: new FormControl(''),
+      lrem8: new FormControl(''),
+      lobs9: new FormControl(''),
+      lrem9: new FormControl(''),
+      lobs10: new FormControl(''),
+      lrem10: new FormControl(''),
+      lobs11: new FormControl(''),
+      lrem11: new FormControl('')
+    })
+  }
+
   submit(){
       this.downArr = this.downConductorForm.get('downArr') as FormArray;
       this.downArr.push(this.createDownArrForm());
       console.log(this.downConductorForm)
     }
+  submit1(){
+      this.downArr = this.downConductorForm.get('bridgingArr') as FormArray;
+      this.downArr.push(this.createBridgeArrForm());
+      console.log(this.downConductorForm)
+    }
+  submit2(){
+      this.holderArr = this.downConductorForm.get('holderArr') as FormArray;
+      this.holderArr.push(this.createHolderArrForm());
+      console.log(this.downConductorForm)
   }
+  submit3(){
+      this.connectorArr = this.downConductorForm.get('connectorArr') as FormArray;
+      this.connectorArr.push(this.createConnectorArrForm());
+      console.log(this.downConductorForm)
+  }
+  submit4(){
+    this.lightArr = this.downConductorForm.get('lightArr') as FormArray;
+    this.lightArr.push(this.createLightArrForm());
+    console.log(this.downConductorForm)
+}
+}
 
