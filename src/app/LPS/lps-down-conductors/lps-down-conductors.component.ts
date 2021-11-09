e108f28e76c7c9db8b837de8b7c341de89a4563f
Hasan <hasan4java@gmail.com>
2021-11-09 15:02:25 +0530

    DownConductor Module appending to Backend

--- src/app/LPS/lps-down-conductors/lps-down-conductors.component.ts
+++ src/app/LPS/lps-down-conductors/lps-down-conductors.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { DownConductorDescription } from 'src/app/LPS_model/down-conductor';
+import { LpsDownconductorService } from 'src/app/LPS_services/lps-downconductor.service';
 
 @Component({
   selector: 'app-lps-down-conductors',
@@ -8,198 +10,288 @@ import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup } from 
 })
 export class LpsDownConductorsComponent implements OnInit {
 
-  downConductorForm! : FormGroup;
-  
-  downArr! : FormArray;
+  downConductorForm!: FormGroup;
+
+  downArr!: FormArray;
   bridgingArr!: FormArray;
   holderArr!: FormArray;
   connectorArr!: FormArray;
   lightArr!: FormArray;
+  testjointsArr!: FormArray;
+  lpsDownconductorService;
+  submitted = false;
+  downConductorDescription= new DownConductorDescription();
 
   constructor(
-    private formBuilder : FormBuilder,
-  ) { }
+    private formBuilder: FormBuilder, lpsDownconductorService: LpsDownconductorService) {
+    this.lpsDownconductorService = lpsDownconductorService
+  }
 
   ngOnInit(): void {
     this.downConductorForm = this.formBuilder.group({
-    downArr: this.formBuilder.array([this.createDownArrForm()]),
-    bridgingArr: this.formBuilder.array([this.createBridgeArrForm()]),
-    holderArr: this.formBuilder.array([this.createHolderArrForm()]),
-    connectorArr: this.formBuilder.array([this.createConnectorArrForm()]),
-    lightArr: this.formBuilder.array([this.createLightArrForm()])
-  });
+
+      biMetallicIssueOb: new FormControl(''),
+      biMetallicIssueRem: new FormControl(''),
+      warningNoticeGroundLevelOb: new FormControl(''),
+      warningNoticeGroundLevelRem: new FormControl(''),
+      noPowerDownConductorOb: new FormControl(''),
+      noPowerDownConductorRem: new FormControl(''),
+      connectMadeBrazingOb: new FormControl(''),
+      connectMadeBrazingRem: new FormControl(''),
+      chemicalSprinklerOb: new FormControl(''),
+      chemicalSprinklerRem: new FormControl(''),
+      cobustMaterialWallOB: new FormControl(''),
+      cobustMaterialWallRem: new FormControl(''),
+
+      downArr: this.formBuilder.array([this.createDownArrForm()]),
+      bridgingArr: this.formBuilder.array([this.createBridgeArrForm()]),
+      holderArr: this.formBuilder.array([this.createHolderArrForm()]),
+      connectorArr: this.formBuilder.array([this.createConnectorArrForm()]),
+      lightArr: this.formBuilder.array([this.createLightArrForm()]),
+      testjointsArr: this.formBuilder.array([this.createTestJointsArrForm()])
+    });
   }
 
   downConductorControls(): AbstractControl[] {
-    return (<FormArray> this.downConductorForm.get('downArr')).controls
+    return (<FormArray>this.downConductorForm.get('downArr')).controls
   }
 
   bridgingCablesControls(): AbstractControl[] {
-    return (<FormArray> this.downConductorForm.get('bridgingArr')).controls
+    return (<FormArray>this.downConductorForm.get('bridgingArr')).controls
   }
 
   holdersControls(): AbstractControl[] {
-    return (<FormArray> this.downConductorForm.get('holderArr')).controls
+    return (<FormArray>this.downConductorForm.get('holderArr')).controls
   }
 
   connectorsControls(): AbstractControl[] {
-    return (<FormArray> this.downConductorForm.get('connectorArr')).controls
+    return (<FormArray>this.downConductorForm.get('connectorArr')).controls
   }
 
   lightControls(): AbstractControl[] {
-    return (<FormArray> this.downConductorForm.get('lightArr')).controls
+    return (<FormArray>this.downConductorForm.get('lightArr')).controls
   }
+
+  jointControls(): AbstractControl[] {
+    return (<FormArray>this.downConductorForm.get('testjointsArr')).controls
+  }
+
   private createDownArrForm(): FormGroup {
 
     return new FormGroup({
-      description: new FormControl(''),
-      remarks: new FormControl(''),
-      materialDes: new FormControl(''),
-      materialRemarks: new FormControl(''),
-      sideDes: new FormControl(''),
-      sideRemarks: new FormControl(''),
-      insulatedDes: new FormControl(''),
-      insulatedRemarks: new FormControl(''),
-      downDes: new FormControl(''),
-      downRemarks: new FormControl(''),
-      gutterDes: new FormControl(''),
-      gutterRemarks: new FormControl(''),
-      ensureDes: new FormControl(''),
-      ensureRemarks: new FormControl(''),
-      equipDes: new FormControl(''),
-      equipRemarks: new FormControl(''),
-      perimeterDes: new FormControl(''),
-      perimeterRemarks: new FormControl(''),
-      maxDes: new FormControl(''),
-      maxRemarks: new FormControl(''),
-      miniDes: new FormControl(''),
-      miniRemarks: new FormControl(''),
-      totalDes: new FormControl(''),
-      totalRemarks: new FormControl(''),
-      numberDes: new FormControl(''),
-      numberRemarks: new FormControl(''),
-      passedDes: new FormControl(''),
-      passedRemarks: new FormControl(''),
-      failedDes: new FormControl(''),
-      failedRemarks: new FormControl('')
+      lacationNo: new FormControl(''),
+      lacationName: new FormControl(''),
+      physicalInspectionOb: new FormControl(''),
+      physicalInspectionRem: new FormControl(''),
+      conductMaterialOb: new FormControl(''),
+      conductMaterialRem: new FormControl(''),
+      conductSizeOb: new FormControl(''),
+      conductSizeRem: new FormControl(''),
+      downConductExposedOb: new FormControl(''),
+      downConductExposedRem: new FormControl(''),
+      downConductLocationdOb: new FormControl(''),
+      downConductLocationdRem: new FormControl(''),
+      downConductGutterOb: new FormControl(''),
+      downConductGutterRem: new FormControl(''),
+      ensureDownCnoductOb: new FormControl(''),
+      ensureDownCnoductRem: new FormControl(''),
+      installationDownConductOb: new FormControl(''),
+      installationDownConductRem: new FormControl(''),
+      maximumDownConductOb: new FormControl(''),
+      maximumDownConductRem: new FormControl(''),
+      manimumDownConductOb: new FormControl(''),
+      manimumDownConductRem: new FormControl(''),
+      totalNoDownConductOb: new FormControl(''),
+      totalNoDownConductRem: new FormControl(''),
+      inspectedNoOb: new FormControl(''),
+      inspectedNoRem: new FormControl(''),
+      inspectionPassedNoOb: new FormControl(''),
+      inspectionPassedNoRem: new FormControl(''),
+      inspectionFailedNoOb: new FormControl(''),
+      inspectionFailedNoRem: new FormControl('')
     })
   }
 
   private createBridgeArrForm(): FormGroup {
-    
+
     return new FormGroup({
-      obs1: new FormControl(''),
-      rem1: new FormControl(''),
-      obs2: new FormControl(''),
-      rem2: new FormControl(''),
-      obs3: new FormControl(''),
-      rem3: new FormControl(''),
-      obs4: new FormControl(''),
-      rem4: new FormControl(''),
-      obs5: new FormControl(''),
-      rem5: new FormControl(''),
-      obs6: new FormControl(''),
-      rem6: new FormControl(''),
-      obs7: new FormControl(''),
-      rem7: new FormControl('')
+      lacationNo: new FormControl(''),
+      lacationName: new FormControl(''),
+      ensureBridgingCableOb: new FormControl(''),
+      ensureBridgingCableRem: new FormControl(''),
+      aluminiumConductorSideWallOb: new FormControl(''),
+      aluminiumConductorSideWallRem: new FormControl(''),
+      bridgingCableConnectionOb: new FormControl(''),
+      bridgingCableConnectionRem: new FormControl(''),
+      totalNoBridgingCableOb: new FormControl(''),
+      totalNoBridgingCableRem: new FormControl(''),
+      inspectedNoOb: new FormControl(''),
+      inspectedNoRem: new FormControl(''),
+      inspectionPassedNoOb: new FormControl(''),
+      inspectionPassedNoRem: new FormControl(''),
+      inspectionFailedNoOb: new FormControl(''),
+      inspectionFailedNoRem: new FormControl('')
     })
   }
 
   private createHolderArrForm(): FormGroup {
-    
+
     return new FormGroup({
-      holderObs1: new FormControl(''),
-      holderRem1: new FormControl(''),
-      holderObs2: new FormControl(''),
-      holderRem2: new FormControl(''),
-      holderObs3: new FormControl(''),
-      holderRem3: new FormControl(''),
-      holderObs4: new FormControl(''),
-      holderRem4: new FormControl(''),
-      holderObs5: new FormControl(''),
-      holderRem5: new FormControl(''),
-      holderObs6: new FormControl(''),
-      holderRem6: new FormControl(''),
-      holderObs7: new FormControl(''),
-      holderRem7: new FormControl(''),
-      holderObs8: new FormControl(''),
-      holderRem8: new FormControl('')
+      physicalInspectionOb: new FormControl(''),
+      physicalInspectionRem: new FormControl(''),
+      conductHolderFlatSurfaceOb: new FormControl(''),
+      conductHolderFlatSurfaceRem: new FormControl(''),
+      conductorHoldedOb: new FormControl(''),
+      conductorHoldedRem: new FormControl(''),
+      materialHolderOb: new FormControl(''),
+      materialHolderRem: new FormControl(''),
+      totalNoHolderOb: new FormControl(''),
+      totalNoHolderRem: new FormControl(''),
+      inspectedNoOb: new FormControl(''),
+      inspectedNoRem: new FormControl(''),
+      inspectionPassedNoOb: new FormControl(''),
+      inspectionPassedNoRem: new FormControl(''),
+      inspectionFailedNoOb: new FormControl(''),
+      inspectionFailedNoRem: new FormControl('')
     })
   }
 
   private createConnectorArrForm(): FormGroup {
-    
+
     return new FormGroup({
-      cobs1: new FormControl(''),
-      crem1: new FormControl(''),
-      cobs2: new FormControl(''),
-      crem2: new FormControl(''),
-      cobs3: new FormControl(''),
-      crem3: new FormControl(''),
-      cobs4: new FormControl(''),
-      crem4: new FormControl(''),
-      cobs5: new FormControl(''),
-      crem5: new FormControl(''),
-      cobs6: new FormControl(''),
-      crem6: new FormControl(''),
-      cobs7: new FormControl(''),
-      crem7: new FormControl(''),
-      cobs8: new FormControl(''),
-      crem8: new FormControl('')
+      physicalInspectionOb: new FormControl(''),
+      physicalInspectionRem: new FormControl(''),
+      strightConnectCheckOb: new FormControl(''),
+      strightConnectCheckRem: new FormControl(''),
+      materialConnectorOb: new FormControl(''),
+      materialConnectorRem: new FormControl(''),
+      maxConnectorsDownConductorOb: new FormControl(''),
+      maxConnectorsDownConductorRem: new FormControl(''),
+      totalNoConnectorsOb: new FormControl(''),
+      totalNoConnectorsRem: new FormControl(''),
+      inspectedNoOb: new FormControl(''),
+      inspectedNoRem: new FormControl(''),
+      inspectionPassedNoOb: new FormControl(''),
+      inspectionPassedNoRem: new FormControl(''),
+      inspectionFailedNoOb: new FormControl(''),
+      inspectionFailedNoRem: new FormControl('')
     })
   }
 
   private createLightArrForm(): FormGroup {
+   
     return new FormGroup({
-      lobs1: new FormControl(''),
-      lrem1: new FormControl(''),
-      lobs2: new FormControl(''),
-      lrem2: new FormControl(''),
-      lobs3: new FormControl(''),
-      lrem3: new FormControl(''),
-      lobs4: new FormControl(''),
-      lrem4: new FormControl(''),
-      lobs5: new FormControl(''),
-      lrem5: new FormControl(''),
-      lobs6: new FormControl(''),
-      lrem6: new FormControl(''),
-      lobs7: new FormControl(''),
-      lrem7: new FormControl(''),
-      lobs8: new FormControl(''),
-      lrem8: new FormControl(''),
-      lobs9: new FormControl(''),
-      lrem9: new FormControl(''),
-      lobs10: new FormControl(''),
-      lrem10: new FormControl(''),
-      lobs11: new FormControl(''),
-      lrem11: new FormControl('')
+      threadHoldCurrentOb: new FormControl(''),
+      threadHoldCurrentRem: new FormControl(''),
+      maximumWithStandCurrentOb: new FormControl(''),
+      maximumWithStandCurrentRem: new FormControl(''),
+      countsOb: new FormControl(''),
+      countsRem: new FormControl(''),
+      batteryLifeTimeOb: new FormControl(''),
+      batteryLifeTimeRem: new FormControl(''),
+      properConnectionLightingCounterOb: new FormControl(''),
+      properConnectionLightingCounterRem: new FormControl(''),
+      lightingCounterPlacedOb: new FormControl(''),
+      lightingCounterPlacedRem: new FormControl(''),
+      conditionOfLightingCounterOb: new FormControl(''),
+      conditionOfLightingCounterRem: new FormControl(''),
+      totalNoLightingCounterOb: new FormControl(''),
+      totalNoLightingCounterRem: new FormControl(''),
+      inspectedNoOb: new FormControl(''),
+      inspectedNoRem: new FormControl(''),
+      inspectionPassedNoOb: new FormControl(''),
+      inspectionPassedNoRem: new FormControl(''),
+      inspectionFailedNoOb: new FormControl(''),
+      inspectionFailedNoRem: new FormControl('')
     })
   }
 
-  submit(){
-      this.downArr = this.downConductorForm.get('downArr') as FormArray;
-      this.downArr.push(this.createDownArrForm());
-      console.log(this.downConductorForm)
-    }
-  submit1(){
-      this.downArr = this.downConductorForm.get('bridgingArr') as FormArray;
-      this.downArr.push(this.createBridgeArrForm());
-      console.log(this.downConductorForm)
-    }
-  submit2(){
-      this.holderArr = this.downConductorForm.get('holderArr') as FormArray;
-      this.holderArr.push(this.createHolderArrForm());
-      console.log(this.downConductorForm)
+  private createTestJointsArrForm(): FormGroup {
+  
+    return new FormGroup({
+      lacationNo: new FormControl(''),
+      lacationName: new FormControl(''),
+      testJointTypeOb: new FormControl(''),
+      testJointTypeRem: new FormControl(''),
+      materialTestJointOb: new FormControl(''),
+      materialTestJointRem: new FormControl(''),
+      accessibilityOfTestJointOb: new FormControl(''),
+      accessibilityOfTestJointRem: new FormControl(''),
+      nonMetalicProtectionOb: new FormControl(''),
+      nonMetalicProtectionRem: new FormControl(''),
+      testJointPlacedGroungLevelOb: new FormControl(''),
+      testJointPlacedGroungLevelRem: new FormControl(''),
+      bimetallicIssueCheckOb: new FormControl(''),
+      bimetallicIssueCheckRem: new FormControl(''),
+      totalNoOfTestJointOB: new FormControl(''),
+      totalNoOfTestJointRem: new FormControl(''),
+      inspectedNoOb: new FormControl(''),
+      inspectedNoRem: new FormControl(''),
+      inspectionPassedNoOb: new FormControl(''),
+      inspectionPassedNoRem: new FormControl(''),
+      inspectionFailedNoOb: new FormControl(''),
+      inspectionFailedNoRem: new FormControl('')
+    })
   }
-  submit3(){
-      this.connectorArr = this.downConductorForm.get('connectorArr') as FormArray;
-      this.connectorArr.push(this.createConnectorArrForm());
-      console.log(this.downConductorForm)
+
+  submit() {
+    this.downArr = this.downConductorForm.get('downArr') as FormArray;
+    this.downArr.push(this.createDownArrForm());
+    console.log(this.downConductorForm)
   }
-  submit4(){
+  submit1() {
+    this.downArr = this.downConductorForm.get('bridgingArr') as FormArray;
+    this.downArr.push(this.createBridgeArrForm());
+    console.log(this.downConductorForm)
+  }
+  submit2() {
+    this.holderArr = this.downConductorForm.get('holderArr') as FormArray;
+    this.holderArr.push(this.createHolderArrForm());
+    console.log(this.downConductorForm)
+  }
+  submit3() {
+    this.connectorArr = this.downConductorForm.get('connectorArr') as FormArray;
+    this.connectorArr.push(this.createConnectorArrForm());
+    console.log(this.downConductorForm)
+  }
+  submit4() {
     this.lightArr = this.downConductorForm.get('lightArr') as FormArray;
     this.lightArr.push(this.createLightArrForm());
     console.log(this.downConductorForm)
+  }
+
+   onSubmit() {
+    this.submitted=true;
+this.downConductorDescription.userName="inspector2@gmail.com";
+this.downConductorDescription.basicLpsId=22;
+if (this.downConductorForm.invalid) {
+
+  return;
+
 }
+this.downConductorDescription.downConductor = this.downConductorForm.value.downArr;
+this.downConductorDescription.bridgingDescription = this.downConductorForm.value.bridgingArr;
+this.downConductorDescription.holder = this.downConductorForm.value.holderArr;
+this.downConductorDescription.connectors = this.downConductorForm.value.connectorArr;
+this.downConductorDescription.lightningCounter = this.downConductorForm.value.lightArr;
+this.downConductorDescription.testingJoint = this.downConductorForm.value.testjointsArr;
+
+
+    this.lpsDownconductorService.saveDownConductors(this.downConductorDescription).subscribe(
+
+      () => {
+
+         
+      
+      },
+      () => {
+      }
+    )
+    console.log(this.downConductorDescription)
+  }
+
+  get f() {
+    return this.downConductorForm.controls;
+  }
 }
 
---
+++ src/app/LPS_model/down-conductor.ts
@@ -0,0 +1,165 @@
+export class DownConductorDescription {
+    basicLpsId!: number;
+    userName!: String;
+    biMetallicIssueOb!: String;
+    biMetallicIssueRem!: String;
+    warningNoticeGroundLevelOb!: String;
+    warningNoticeGroundLevelRem!: String;
+    noPowerDownConductorOb!: String;
+    noPowerDownConductorRem!: String;
+    connectMadeBrazingOb!: String;
+    connectMadeBrazingRem!: String;
+    chemicalSprinklerOb!: String;
+    chemicalSprinklerRem!: String;
+    cobustMaterialWallOB!: String;
+    cobustMaterialWallRem!: String;
+
+    downConductor!: DownConductor[];
+    bridgingDescription!: BridgingDescription[];
+    holder!: Holder[];
+    connectors!: Connectors[];
+    lightningCounter!: LightningCounter[];
+    testingJoint!: TestingJoint[];
+}
+
+export class DownConductor {
+    lacationNo!:number;
+    lacationName!:String;
+    physicalInspectionOb!: String;
+    physicalInspectionRem!: String;
+    conductMaterialOb!: String;
+    conductMaterialRem!: String;
+    conductSizeOb!: String;
+    conductSizeRem!: String;
+    downConductExposedOb!: String;
+    downConductExposedRem!: String;
+    downConductLocationdOb!: String;
+    downConductLocationdRem!: String;
+    downConductGutterOb!: String;
+    downConductGutterRem!: String;
+    ensureDownCnoductOb!: String;
+    ensureDownCnoductRem!: String;
+    installationDownConductOb!: String;
+    installationDownConductRem!: String;
+    maximumDownConductOb!: String;
+    maximumDownConductRem!: String;
+    manimumDownConductOb!: String;
+    manimumDownConductRem!: String;
+    totalNoDownConductOb!: String;
+    totalNoDownConductRem!: String;
+    inspectedNoOb!: String;
+    inspectedNoRem!: String;
+    inspectionPassedNoOb!: String;
+    inspectionPassedNoRem!: String;
+    inspectionFailedNoOb!: String;
+    inspectionFailedNoRem!: String;
+}
+
+export class BridgingDescription {
+    lacationNo!: number;
+    lacationName!: String;
+    ensureBridgingCableOb!: String;
+    ensureBridgingCableRem!: String;
+    aluminiumConductorSideWallOb!: String;
+    aluminiumConductorSideWallRem!: String;
+    bridgingCableConnectionOb!: String;
+    bridgingCableConnectionRem!: String;
+    totalNoBridgingCableOb!: String;
+    totalNoBridgingCableRem!: String;
+    inspectedNoOb!: String;
+    inspectedNoRem!: String;
+    inspectionPassedNoOb!: String;
+    inspectionPassedNoRem!: String;
+    inspectionFailedNoOb!: String;
+    inspectionFailedNoRem!: String;
+}
+
+export class Holder {
+    physicalInspectionOb!: String;
+    physicalInspectionRem!: String;
+    conductHolderFlatSurfaceOb!: String;
+    conductHolderFlatSurfaceRem!: String;
+    conductorHoldedOb!: String;
+    conductorHoldedRem!: String;
+    materialHolderOb!: String;
+    materialHolderRem!: String;
+    totalNoHolderOb!: String;
+    totalNoHolderRem!: String;
+    inspectedNoOb!: String;
+    inspectedNoRem!: String;
+    inspectionPassedNoOb!: String;
+    inspectionPassedNoRem!: String;
+    inspectionFailedNoOb!: String;
+    inspectionFailedNoRem!: String;
+}
+
+export class Connectors {
+    physicalInspectionOb!: String;
+    physicalInspectionRem!: String;
+    strightConnectCheckOb!: String;
+    strightConnectCheckRem!: String;
+    materialConnectorOb!: String;
+    materialConnectorRem!: String;
+    maxConnectorsDownConductorOb!: String;
+    maxConnectorsDownConductorRem!: String;
+    totalNoConnectorsOb!: String;
+    totalNoConnectorsRem!: String;
+    inspectedNoOb!: String;
+    inspectedNoRem!: String;
+    inspectionPassedNoOb!: String;
+    inspectionPassedNoRem!: String;
+    inspectionFailedNoOb!: String;
+    inspectionFailedNoRem!: String;
+}
+
+
+export class LightningCounter {
+    threadHoldCurrentOb!: String;
+    threadHoldCurrentRem!: String;
+    maximumWithStandCurrentOb!: String;
+    maximumWithStandCurrentRem!: String;
+    countsOb!: String;
+    countsRem!: String;
+    batteryLifeTimeOb!: String;
+    batteryLifeTimeRem!: String;
+    properConnectionLightingCounterOb!: String;
+    properConnectionLightingCounterRem!: String;
+    lightingCounterPlacedOb!: String;
+    lightingCounterPlacedRem!: String;
+    conditionOfLightingCounterOb!: String;
+    conditionOfLightingCounterRem!: String;
+    totalNoLightingCounterOb!: String;
+    totalNoLightingCounterRem!: String;
+    inspectedNoOb!: String;
+    inspectedNoRem!: String;
+    inspectionPassedNoOb!: String;
+    inspectionPassedNoRem!: String;
+    inspectionFailedNoOb!: String;
+    inspectionFailedNoRem!: String;
+}
+
+export class TestingJoint {
+    lacationNo!: number;
+    lacationName!: String;
+    testJointTypeOb!: String;
+    testJointTypeRem!: String;
+    materialTestJointOb!: String;
+    materialTestJointRem!: String;
+    accessibilityOfTestJointOb!: String;
+    accessibilityOfTestJointRem!: String;
+    nonMetalicProtectionOb!: String;
+    nonMetalicProtectionRem!: String;
+    testJointPlacedGroungLevelOb!: String;
+    testJointPlacedGroungLevelRem!: String;
+    bimetallicIssueCheckOb!: String;
+    bimetallicIssueCheckRem!: String;
+    totalNoOfTestJointOB!: String;
+    totalNoOfTestJointRem!: String;
+    inspectedNoOb!: String;
+    inspectedNoRem!: String;
+    inspectionPassedNoOb!: String;
+    inspectionPassedNoRem!: String;
+    inspectionFailedNoOb!: String;
+    inspectionFailedNoRem!: String;
+}
+
