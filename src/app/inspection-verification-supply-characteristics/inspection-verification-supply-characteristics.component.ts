f243010f2d69f31c72331ab5362452dacb9a2e38
capeelangovan <86285794+capeelangovan@users.noreply.github.com>
2021-12-20 17:41:41 +0530

    Step 2 issues new update for alternate supply

--- src/app/inspection-verification-supply-characteristics/inspection-verification-supply-characteristics.component.ts
+++ src/app/inspection-verification-supply-characteristics/inspection-verification-supply-characteristics.component.ts
@@ -456,6 +456,18 @@ export class InspectionVerificationSupplyCharacteristicsComponent
    this.expandedIndex = -1 ;
   }
 
+    // Only Accept numbers
+    keyPressNumbers(event:any) {
+      var charCode = (event.which) ? event.which : event.keyCode;
+      // Only Numbers 0-9
+        if ((charCode < 48 || charCode > 57)) {
+          event.preventDefault();
+          return false;
+        } else {
+          return true;
+        }
+    }
+
   onKeyMainShortName(e: any){
     let values = e.target.value;
     // this.alternateArr = this.supplycharesteristicForm.get(
@@ -2204,7 +2216,7 @@ showHideAccordion(index: number) {
       }
     } 
     else if (this.location2Arr.length > this.value && this.value != 0) {
-     debugger
+     
       if (this.value != '') {
        
         this.delarr = this.location2Arr.length - this.value;
@@ -2599,6 +2611,7 @@ showHideAccordion(index: number) {
   }
 
   onKeyAlernate(event: KeyboardEvent) {
+    debugger
     this.values = (<HTMLInputElement>event.target).value;
     this.value = this.values;
     this.alternateArr = this.supplycharesteristicForm.get(
@@ -2615,8 +2628,14 @@ showHideAccordion(index: number) {
       this.circuitArr = this.supplycharesteristicForm.get(
         'circuitArr'
       ) as FormArray;
-      if (this.alternateArr.length == 1) {
-        for (this.i = 1; this.i < this.value; this.i++) {
+      //if (this.alternateArr.length == 1 || this.alternateArr.length < this.value) {
+        let c = 0;
+        if(this.alternateArr.length < this.value){
+          if(this.alternateArr.length != 0){
+            c =this.alternateArr.length;
+          }
+        for (this.i = c; this.i < this.value; this.i++) {
+
           this.alternateArr.push(this.SupplyparametersForm());
           this.circuitArr.push(this.createCircuitForm());
         }
@@ -2624,6 +2643,7 @@ showHideAccordion(index: number) {
         this.breaker = true;
       } 
       else if ((this.alternateArr.length < this.value) && this.alternateArr.length!=0)  {
+        debugger
         if (this.value != '') {
           this.delarr = this.value - this.alternateArr.length;
           this.delarr1 = this.delarr;
@@ -2644,6 +2664,7 @@ showHideAccordion(index: number) {
       } 
       else if((this.alternateArr.length > this.value))
       {
+        debugger
         if (this.value != '') {
           this.delarr = this.alternateArr.length - this.value;
           this.delarr1 = this.delarr;
@@ -2663,14 +2684,15 @@ showHideAccordion(index: number) {
         }
       }
       else {
-      //  if(!this.alternateArr.length == this.value){ removed from elango's yesterday commit by Aish
+        debugger
+        if(!this.alternateArr.length == this.value){ //removed from elango's yesterday commit by Aish
         for (this.i = 0; this.i < this.value; this.i++) {
           this.alternateArr.push(this.SupplyparametersForm());
           this.circuitArr.push(this.createCircuitForm());
         }
         this.sources = true;
         this.breaker = true;
-     // }
+       }
     }
     } 
     // else if (this.value == '') {
@@ -3449,30 +3471,30 @@ else{
       );
   }
   }
-
-  removeItem(a:any,index: any) {
-   
+  
+   removeItem(a:any,index: any) {
     this.supplycharesteristicForm.markAsTouched();
-    if(a.value.locationReportId !=0 && a.value.locationReportId !=undefined)
-    {
-      this.supplycharesteristic.noOfLocation = this.supplycharesteristicForm.value.noOfLocation -1;
-      (this.supplycharesteristicForm.get('location1Arr') as FormArray).removeAt(index);
-      a.value.instalLocationReportStatus='R';
-      this.locationArr= this.locationArr.concat(a.value);
-      this.supplycharesteristicForm.markAsDirty();
-      if(this.supplycharesteristic.noOfLocation == 0){
-        this.key1LocationTable = false;
+    if(this.supplycharesteristic.noOfLocation != 0){
+      if(a.value.locationReportId !=0 && a.value.locationReportId !=undefined)
+      {
+        this.supplycharesteristic.noOfLocation = this.supplycharesteristicForm.value.noOfLocation -1;
+        (this.supplycharesteristicForm.get('location1Arr') as FormArray).removeAt(index);
+        a.value.instalLocationReportStatus='R';
+        this.locationArr= this.locationArr.concat(a.value);
+        this.supplycharesteristicForm.markAsDirty();
+        if(this.supplycharesteristic.noOfLocation == 0){
+          this.key1LocationTable = false;
+        }
       }
-    }
-    else
-    {
-     
-      this.supplycharesteristicForm.markAsTouched();
-      this.supplycharesteristic.noOfLocation = this.supplycharesteristicForm.value.noOfLocation -1;
-      (this.supplycharesteristicForm.get('location1Arr') as FormArray).removeAt(index);
-      this.supplycharesteristicForm.markAsDirty();
-      if(this.supplycharesteristic.noOfLocation == 0){
-        this.key1LocationTable = false;
+      else
+      {
+        this.supplycharesteristicForm.markAsTouched();
+        this.supplycharesteristic.noOfLocation = this.supplycharesteristicForm.value.noOfLocation -1;
+        (this.supplycharesteristicForm.get('location1Arr') as FormArray).removeAt(index);
+        this.supplycharesteristicForm.markAsDirty();
+        if(this.supplycharesteristic.noOfLocation == 0){
+          this.key1LocationTable = false;
+        }
       }
     }
    }
@@ -3480,26 +3502,28 @@ else{
    removeItem1(a:any,index: any) {
    
     this.supplycharesteristicForm.markAsTouched();
-    if(a.value.locationReportId !=0 && a.value.locationReportId !=undefined)
-    {
-      this.supplycharesteristic.bondingNoOfJoints = this.supplycharesteristicForm.value.bondingNoOfJoints -1;
-      (this.supplycharesteristicForm.get('location2Arr') as FormArray).removeAt(index);
-      a.value.instalLocationReportStatus='R';
-      this.boundingArr= this.boundingArr.concat(a.value);
-      this.supplycharesteristicForm.markAsDirty();
-      if(this.supplycharesteristic.bondingNoOfJoints == 0){
-        this.JointLocationTable = false;
+    if(this.supplycharesteristic.bondingNoOfJoints != 0){
+      if(a.value.locationReportId !=0 && a.value.locationReportId !=undefined)
+      {
+        this.supplycharesteristic.bondingNoOfJoints = this.supplycharesteristicForm.value.bondingNoOfJoints -1;
+        (this.supplycharesteristicForm.get('location2Arr') as FormArray).removeAt(index);
+        a.value.instalLocationReportStatus='R';
+        this.boundingArr= this.boundingArr.concat(a.value);
+        this.supplycharesteristicForm.markAsDirty();
+        if(this.supplycharesteristic.bondingNoOfJoints == 0){
+          this.JointLocationTable = false;
+        }
       }
-    }
-    else
-    {
-      this.supplycharesteristicForm.markAsTouched();
-     
-      this.supplycharesteristic.bondingNoOfJoints = this.supplycharesteristicForm.value.bondingNoOfJoints -1;
-      (this.supplycharesteristicForm.get('location2Arr') as FormArray).removeAt(index);
-      this.supplycharesteristicForm.markAsDirty();
-      if(this.supplycharesteristic.bondingNoOfJoints == 0){
-        this.JointLocationTable = false;
+      else
+      {
+        this.supplycharesteristicForm.markAsTouched();
+       
+        this.supplycharesteristic.bondingNoOfJoints = this.supplycharesteristicForm.value.bondingNoOfJoints -1;
+        (this.supplycharesteristicForm.get('location2Arr') as FormArray).removeAt(index);
+        this.supplycharesteristicForm.markAsDirty();
+        if(this.supplycharesteristic.bondingNoOfJoints == 0){
+          this.JointLocationTable = false;
+        }
       }
     }
    }
@@ -3507,7 +3531,8 @@ else{
    removeItem2(a:any,index: any) {
    
     this.supplycharesteristicForm.markAsTouched();
-    if(a.value.locationReportId !=0 && a.value.locationReportId !=undefined)
+    if(this.supplycharesteristic.earthingNoOfJoints != 0){
+      if(a.value.locationReportId !=0 && a.value.locationReportId !=undefined)
     {
       this.supplycharesteristic.earthingNoOfJoints = this.supplycharesteristicForm.value.earthingNoOfJoints -1;
       (this.supplycharesteristicForm.get('location3Arr') as FormArray).removeAt(index);
@@ -3521,7 +3546,6 @@ else{
     else
     {
       this.supplycharesteristicForm.markAsTouched();
-     
       this.supplycharesteristic.earthingNoOfJoints = this.supplycharesteristicForm.value.earthingNoOfJoints -1;
       (this.supplycharesteristicForm.get('location3Arr') as FormArray).removeAt(index);
       this.supplycharesteristicForm.markAsDirty();
@@ -3529,11 +3553,10 @@ else{
         this.keyJOintLocationTable = false;
       }
     }
+    }
    }
-
     //  Allternative power supply
     removeItem3(a:any,index: any) {
-
       this.supplycharesteristicForm.markAsTouched();
       if(a.value.supplyparametersId !=0 && a.value.supplyparametersId !=undefined)
       {
@@ -3550,14 +3573,14 @@ else{
           
           for (let i = 0; i < items.length; i++) {
               
-          if(i==index){
-            
-            let d = items.value[i+1];
-            d.circuitStatus ='R';
-            this.circuitArr1 = this.circuitArr1.concat(d);
-            (this.supplycharesteristicForm.get('circuitArr') as FormArray).removeAt(i+1);
+            let d = items.value[i];
+            if(a.value.aLSupplyShortName == d.sourceName){
+              d.circuitStatus ='R';
+              this.circuitArr1 = this.circuitArr1.concat(d);
+              (this.supplycharesteristicForm.get('circuitArr') as FormArray).removeAt(i);
+            }
           }
-        }
+        
         this.supplycharesteristicForm.markAsDirty();
       }
       else
@@ -3566,12 +3589,10 @@ else{
         this.supplycharesteristicForm.markAsTouched();
         let b = parseInt(this.supplycharesteristicForm.value.supplyNumber) -1;
         this.supplycharesteristic.supplyNumber = b.toString();
-        (this.supplycharesteristicForm.get('circuitArr') as FormArray).removeAt(index);
+        (this.supplycharesteristicForm.get('circuitArr') as FormArray).removeAt(index+1);
         (this.supplycharesteristicForm.get('alternateArr') as FormArray).removeAt(index);
         this.supplycharesteristicForm.markAsDirty();
       }
      }
-
-
   
 }
