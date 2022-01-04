7a1131291e56c6cbed2036d1ff07ca2ae661a491
capeelangovan <86285794+capeelangovan@users.noreply.github.com>
2022-01-04 10:52:23 +0530

    lps basic page changes reverted

--- src/app/LPS/lps-basic-page/lps-basic-page.component.ts
+++ src/app/LPS/lps-basic-page/lps-basic-page.component.ts
@@ -331,7 +331,7 @@ export class LpsBasicPageComponent implements OnInit {
           
           this.basicDetails.basicLpsId=basicDetailsItr.basicLpsId;
           this.success = true;
-          this.successMsg = data;
+          this.successMsg = "Basic Information sucessfully Saved";
           //this.disable = true;
           this.retriveBasicDetails();
           this.LPSBasicForm.markAsPristine();
