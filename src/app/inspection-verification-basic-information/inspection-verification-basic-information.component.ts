798d1eb82c5806146577c6067a9968470f69d858
CAPE-aishwarya <84174657+CAPE-aishwarya@users.noreply.github.com>
2022-02-08 19:24:17 +0530

    Update inspection-verification-basic-information.component.ts

--- src/app/inspection-verification-basic-information/inspection-verification-basic-information.component.ts
+++ src/app/inspection-verification-basic-information/inspection-verification-basic-information.component.ts
@@ -1776,21 +1776,21 @@ onPopState(event:any) {
 
      if(this.step1Form.touched || this.step1Form.untouched){
        if(this.service.sigInput==1){
-        this.modalReference=this.modalService.open(content1, { centered: true,backdrop: 'static'});
-        this.modalReference.close();
+        this.modalService.open(content1, { centered: true,backdrop: 'static'});
+        //this.modalReference.close();
        }
        else{
-      this.modalReference = this.modalService.open(content2, {
+      this.modalService.open(content2, {
          centered: true, 
          size: 'md',
          backdrop: 'static'
         });
-        this.modalReference.close();
+       // this.modalReference.close();
       }
      }
      if(this.step1Form.dirty && this.step1Form.touched){ //update
-      this.modalReference=this.modalService.open(content1, { centered: true,backdrop: 'static'});
-      this.modalReference.close();
+      this.modalService.open(content1, { centered: true,backdrop: 'static'});
+      //this.modalReference.close();
      }
     }
    
