1c80a5953bffea74ff6d5005ddd06696201c26a1
capeelangovan <86285794+capeelangovan@users.noreply.github.com>
2022-04-05 22:17:33 +0530

    Update savedreports.component.ts

--- src/app/savedreports/savedreports.component.ts
+++ src/app/savedreports/savedreports.component.ts
@@ -73,7 +73,6 @@ export class SavedreportsComponent implements OnInit {
     this.superAdminArr.push('gk@capeindia.net');
     this.superAdminArr.push('vinoth@capeindia.net');
     this.superAdminArr.push('awstesting@rushforsafety.com');
-    this.superAdminArr.push('elangovan.photonx@gmail.com');
     
     this.retrieveSiteDetails();
    setTimeout(() => this.input.focus(), 500);
