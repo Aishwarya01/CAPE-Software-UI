b24a64041a1f0a8c2b2cea2c89ee647fce4f287f
capeelangovan <86285794+capeelangovan@users.noreply.github.com>
2022-04-07 12:36:17 +0530

    Revert "super admin for lps-summary submit button"
    This reverts commit 021a79e25e8d5c7e5260ee9152e03e2231a2e945.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
-
   production: true,
   environmentName: 'Production',
   apiUrl: 'https://prod.lv.safelv.net/api/v1',
@@ -8,19 +7,6 @@ export const environment = {
   apiUrl_EMC: 'https://prod.emc.safelv.net/api/emc/v1',
   sessionTimeOut: 3600,
   hoursOfGettingApproved: 2,
-  signInContent: 'IEC 60364 gives the rules for the design, erection, and verification of electrical installations up to 1000 V AC and 1500 V DC. These rules are adopted/followed worldwide as wiring rules. Some of the adopted national versions of this standard are BS7671 in UK, VDE0100 in Germany … etc. The NFPA 70 (NEC) of USA closely follows the fundamental safety measures recommended in IEC 60364.  In India the standard is adopted as IS732 for general installations and Part 3 of National Building Code of India part 3 for special locations.  The rules are intended to provide for the safety of persons, livestock and property against dangers and damage which may arise in the reasonable use of electrical installations and to provide for the proper functioning of those installations. The rules are applicable in design, erection and verification of electrical installations such as residential, commercial, public, industrial, agricultural and horticultural, prefabricated buildings, caravans, caravan sites and similar sites, construction sites, exhibitions, fairs and other installations for temporary purposes, marinas, external lighting and similar installations, medical locations, mobile or transportable units, photovoltaic systems, low-voltage generating sets etc. Verification ensures safety of the installation and its conformity to the standard in design, selection and erection of the installation. Initial and periodic verifications are necessary.',
+  signInContent: 'IEC 60364 gives the rules for the design, erection, and verification of electrical installations up to 1000 V AC and 1500 V DC. These rules are adopted/followed worldwide as wiring rules. Some of the adopted national versions of this standard are BS7671 in UK, VDE0100 in Germany … etc. The NFPA 70 (NEC) of USA closely follows the fundamental safety measures recommended in IEC 60364.  In India the standard is adopted as IS732 for general installations and Part 3 of National Building Code of India part 3 for special locations.  The rules are intended to provide for the safety of persons, livestock and property against dangers and damage which may arise in the reasonable use of electrical installations and to provide for the proper functioning of those installations. The rules are applicable in design, erection and verification of electrical installations such as residential, commercial, public, industrial, agricultural and horticultural, prefabricated buildings, caravans, caravan sites and similar sites, construction sites, exhibitions, fairs and other installations for temporary purposes, marinas, external lighting and similar installations, medical locations, mobile or transportable units, photovoltaic systems, low-voltage generating sets etc. Verification ensures safety of the installation and its conformity to the standard in design, selection and erection of the installation. Initial and periodic verifications are necessary.'
 
-};
-
-// For super admin purpose
-export class LpsSummaryConsts {
-  adminEmail: any=[];  
-  constructor() {
-      this.email();
-  }
-  email(){
-      this.adminEmail[0] = 'awstesting@rushforsafety.com';
-      this.adminEmail[1] = 'gk@capeindia.net';
-      this.adminEmail[2] = 'vinoth@capeindia.net';
-  }       
 };
