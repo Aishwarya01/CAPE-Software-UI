86a09d79db2a58547f7aaa3069213b4ad83a87b0
Arunkumar-CAPE <78420626+Arunkumar-CAPE@users.noreply.github.com>
2022-05-19 18:47:50 +0530

    step 2 html changes

--- src/app/risk-assessment-details/risk-assessment-details.component.ts
+++ src/app/risk-assessment-details/risk-assessment-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-risk-assessment-details',
@@ -6,12 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./risk-assessment-details.component.css']
 })
 export class RiskAssessmentDetailsComponent implements OnInit {
-  
+  step2Form!: FormGroup;
   panelOpenState = false;
-
-  constructor() { }
+  loading = false;
+  submitted = false;
+  validationError = false;
+  validationErrorMsg = '';
+  constructor(private router: ActivatedRoute,
+              private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.step2Form = this.formBuilder.group({
+      location: [''],
+      otherLocation: [''],
+      groundFlashDensity: [''],
+      typeOfBuilding: [''],
+      designation: [''],
+    });
+  }
+
+  onKeyForm(e: any) {
+
+  }
+
+  onChangeForm(e: any) {
+
+  }
+
+  get f():any {
+    return this.step2Form.controls;
+  }
+
+  submit() {
+
   }
 
 }
