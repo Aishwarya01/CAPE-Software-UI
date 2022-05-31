1dd471f55bd09b554f71b0e8ca41a02692ef568a
Arunkumar-CAPE <78420626+Arunkumar-CAPE@users.noreply.github.com>
2022-05-31 18:41:56 +0530

    mcb with rcd changes

--- src/app/services/inspection-verification.service.ts
+++ src/app/services/inspection-verification.service.ts
@@ -94,11 +94,31 @@ export class InspectionVerificationService {
     return this.http.get<any>(this.apiUrl5 + '/symbolList', { responseType: 'text' as 'json' })
   }
 
+
+  //MCB
   public addMCB(MCB: any): Observable<any> {
     return this.http.post<any>(this.apiUrl5 + '/saveMCB', MCB, { responseType: 'text' as 'json' })
   }
 
   public retriveMCB(fileName: any,nodeId: any): Observable<any> {
     return this.http.get<any>(this.apiUrl5 + '/retrieveMCB'+'/'+fileName+'/'+nodeId, { responseType: 'text' as 'json' })
+  }
+
+  public updateMCB(MCB: any): Observable<any> {
+    return this.http.put<any>(this.apiUrl5 + '/updateMCB', MCB, { responseType: 'text' as 'json' })
+  }
+
+  //RCBO or MCB with RCD
+
+  public addRCBO(RCBO: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl5 + '/saveRCBO', RCBO, { responseType: 'text' as 'json' })
+  }
+
+  public retriveRCBO(fileName: any,nodeId: any): Observable<any> {
+    return this.http.get<any>(this.apiUrl5 + '/retrieveRCBO'+'/'+fileName+'/'+nodeId, { responseType: 'text' as 'json' })
+  }
+
+  public updateRCBO(RCBO: any): Observable<any> {
+    return this.http.put<any>(this.apiUrl5 + '/updateRCBO', RCBO, { responseType: 'text' as 'json' })
   }
 }
