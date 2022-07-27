9bb2f015032ccc130bd54379e966bec639650771
Priyanka-CAPE <107191799+Priyanka-CAPE@users.noreply.github.com>
2022-07-27 16:49:25 +0530

    state validation issue changes

--- src/app/site/siteadd/siteadd.component.ts
+++ src/app/site/siteadd/siteadd.component.ts
@@ -137,6 +137,7 @@ export class SiteaddComponent implements OnInit {
   // }
 
   changeCountry(e: any) {
+    this.addSiteForm.controls.state.setValue('');
     let changedValue = e.target.value;
     this.stateList = [];
       for(let arr of this.countryList) {
