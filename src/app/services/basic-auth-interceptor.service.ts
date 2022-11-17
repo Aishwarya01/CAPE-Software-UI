3f0261503e8c5b08b0004d83425a596c72e71942
CapeSathish <77102126+CapeSathish@users.noreply.github.com>
2022-11-17 18:48:35 +0530

    Revert "Initial Commit for  JWT Refresh Token"
    This reverts commit ae6fe2a2a0a965507274857a644e863e6cf99d4e.

--- src/app/services/basic-auth-interceptor.service.ts
+++ src/app/services/basic-auth-interceptor.service.ts
@@ -1,10 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpHeaders, HttpEvent, HttpErrorResponse, HttpClient } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpHeaders, HttpEvent } from '@angular/common/http';
 import { LoginserviceService } from './loginservice.service';
 import { Observable } from 'rxjs/internal/Observable';
-import { catchError, switchMap } from 'rxjs/operators';
-import { environment } from 'src/environments/environment';
-import { throwError } from 'rxjs';
 
 
 @Injectable({
@@ -12,10 +9,7 @@ import { throwError } from 'rxjs';
 })
 export class BasicAuthHtppInterceptorService implements HttpInterceptor {
 
-    apiUrl_v2 = environment.apiUrl_v2;
-    static accessToken: String = '';
-    refresh = false;
-  constructor(private loginService: LoginserviceService, private http: HttpClient) { }
+  constructor(private loginService: LoginserviceService) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     if (this.loginService.isUserLoggedIn() && req.url.indexOf('authentication') === -1) {
@@ -24,22 +18,7 @@ export class BasicAuthHtppInterceptorService implements HttpInterceptor {
                 'Authorization': `Bearer ${this.loginService.token}`
             })
         });
-        return next.handle(authReq).pipe(catchError((error: HttpErrorResponse) => {
-            if(error.status===401 && !this.refresh){
-                return this.http.get<any>(this.apiUrl_v2 + '/refreshToken').pipe(
-                    switchMap((res: any) => {
-                        BasicAuthHtppInterceptorService.accessToken = res.token;
-                        return next.handle(req.clone({
-                            setHeaders: {
-                                'Authorization': `Bearer ${BasicAuthHtppInterceptorService.accessToken}`
-                            }
-                        }))
-                    })
-                )
-            }
-            this.refresh = false;
-            return throwError(() => error)
-        }));
+        return next.handle(authReq);
     } else {
         return next.handle(req);
     }
